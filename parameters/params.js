8570b2cb7ff0b8aa955098aae7ab2adc446e68b7
johnrojas0709 <john.rojas0709@gmail.com>
2024-08-22 12:42:42 -0400

    change secret key

--- parameters/params.js
+++ parameters/params.js
@@ -1,6 +1,6 @@
 //customized module for database connection
-const DATABASECONNECTION = "mongodb+srv://johncito0709:<john0709>@johnrojclus.wr9m7.mongodb.net/?retryWrites=true&w=majority&appName=Johnrojclus"
+//const DATABASECONNECTION = "mongodb+srv://johncito0709:<john0709>@johnrojclus.wr9m7.mongodb.net/?retryWrites=true&w=majority&appName=Johnrojclus"
 
-modelule.exports = {
-    DATABASECONNECTION
-}
+//modelule.exports = {
+//    DATABASECONNECTION
+//}
